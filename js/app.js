8f72bb4dc53c190c686d5efee914d877ca4b53ef
Angelino Valeta <angelinovaleta@gmail.com>
2020-04-25 12:22:31 +0100

    get element addForm and set listening  element with submit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/*Add item  todo */
+const addForm = document.querySelector('.add')
+
+
+addForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const todo = addForm.add.value.trim()
+
+
+})
