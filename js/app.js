75da0527a8b6a3bea1497081bb83b7a0a6035c45
Angelino Valeta <angelinovaleta@gmail.com>
2020-04-25 12:33:26 +0100

    create function generateTemplate

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
 /*Add item  todo */
 const addForm = document.querySelector('.add')
+const list = document.querySelector('.todo')
+
+const generateTemplate = todo =>{
+    
+    const itemListHtml = `
+    <li>
+        <span>${todo}</span>
+        <i class="ion-ios-trash-outline icon"></i>
+    </li>
+    ` 
+    list.innerHTML += itemListHtml
+}
 
 
 addForm.addEventListener('submit', e => {
     e.preventDefault()
 
     const todo = addForm.add.value.trim()
+
+    if(todo){
+        generateTemplate(todo)
+    }
 
 
 })
